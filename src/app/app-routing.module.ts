670d03edc55b0dd7a4d568dacc8ca48070fb3d2d
Lucius Mathametica <52294958+k1n4kut4@users.noreply.github.com>
2019-07-14 15:44:54 +0100

    feat(app): AppRoutingModule updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IngredientsComponent } from './ingredients/ingredients.component';
+import { RecipesComponent } from './recipes/recipes.component'; 
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IngredientsComponent },
+  { path: 'recipes', component: RecipesComponent }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
