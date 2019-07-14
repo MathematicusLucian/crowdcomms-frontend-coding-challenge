4c7263cc17d80d22f088afa98824064e92d50fa7
Lucius Mathametica <52294958+k1n4kut4@users.noreply.github.com>
2019-07-14 15:59:41 +0100

    feat(app): added array of foods, and call to data service, to ingredients component

--- src/app/ingredients/ingredients.component.ts
+++ src/app/ingredients/ingredients.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-ingredients',
@@ -7,9 +9,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IngredientsComponent implements OnInit {
 
-  constructor() { }
+  ingredients: any[] = [
+    {
+      "name": "potato", checked: false
+    },
+    {
+      "name": "onions", checked: false
+    },
+    {
+      "name": "garlic", checked: false
+    },
+    {
+      "name": "apple", checked: false
+    },
+    {
+      "name": "flour", checked: false
+    },
+    {
+      "name": "chicken", checked: false
+    },
+    {
+      "name": "mint", checked: false
+    },
+    {
+      "name": "tomato", checked: false
+    },
+    {
+      "name": "parsley", checked: false
+    },
+    {
+      "name": "peas", checked: false
+    },
+    {
+      "name": "tumeric", checked: false
+    },
+    {
+      "name": "rice", checked: false
+    },
+    {
+      "name": "thyme", checked: false
+    },
+    {
+      "name": "yeast", checked: false
+    },
+    {
+      "name": "egg", checked: false
+    },
+    {
+      "name": "bacon", checked: false
+    },
+    {
+      "name": "olives", checked: false
+    },
+    {
+      "name": "lamb", checked: false
+    },
+    {
+      "name": "lentils", checked: false
+    },
+    {
+      "name": "lemon", checked: false
+    }
+  ];
 
-  ngOnInit() {
+  recipes_returned: Object; 
+
+  constructor(private data: DataService, private router: Router) {}
+
+  ngOnInit() { 
+  }
+
+  chooseRecipe() {
+
+    let chosenIngredients = ["potato"];
+    console.log(chosenIngredients);  
+    this.data.setPuppyDataParams(chosenIngredients);
+    this.router.navigate(['/recipes']); 
+  
   }
 
 }
