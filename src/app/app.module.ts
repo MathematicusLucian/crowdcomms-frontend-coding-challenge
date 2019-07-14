ec1f9eaddc5a089de95e6ec1014690312a2234b4
Lucius Mathametica <52294958+k1n4kut4@users.noreply.github.com>
2019-07-14 15:37:59 +0100

    feat(app): generated component ingredients, recipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IngredientsComponent } from './ingredients/ingredients.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IngredientsComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
